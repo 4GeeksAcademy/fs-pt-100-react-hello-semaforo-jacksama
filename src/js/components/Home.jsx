4436103e5e2d77d26eb9945b402c8d72b3076856
Jacksson Sánchez <info@creativoz.es>
2025-04-15 17:41:49 +0000

    TrafficLightJack

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const Home=() => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		const [selectedColor, setSelectedColor] = useState("yellow");
+
+	return (
+		<div className="card">
+			<div className="traffic-light">
+				<div className={"light red" + ((selectedColor === "red") ? " glow" : "")}></div>
+				<div className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}></div>
+				<div className={"light green" + ((selectedColor === "green") ? " glow" : "")}></div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+
---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+
+
+/*const Color = (props) => {
+
+    const [selected, setSelected] = useState('')
+    const [varName, setVarName] = useState()
+
+   
+    const handleClick = (val) => {
+        setSelected(val)
+    }
+
+    const circle = () => {
+
+        if (selected == 'sin luces') {
+            return <div className={`light bg-${props.color}`}></div>
+        }
+        if (selected == 'luces cortas') {
+            return <i className="fa-solid fa-lightbulb"></i>
+        }
+        if (selected == 'luces largas') {
+            return <i className="fa-solid fa-sun"></i>
+        }
+
+    }
+    return (
+        <div>
+            <h2>El choce tiene seleccionado: {selected}<span>{circle()}</span></h2>
+            <ul>
+                <li className={`${selected==='sin luces' && 'bg-success'}`} onClick={() => handleClick('sin luces')}>sin luces</li>
+                <li className={`${selected==='luces cortas' && 'bg-warning'}`} onClick={() => handleClick('luces cortas')}>luces cortas</li>
+                <li onClick={() => handleClick('luces largas')}>luces largas</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Color;
+
+*/
+
